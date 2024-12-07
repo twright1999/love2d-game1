7db43f8842393b0d0c860c9948e1651c10b50ed0
Oliver Wright <woliver20002@gmail.com>
2024-12-07 18:44:14 +0000

    Added ball

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 ----------------------------------------------
 -- Project: game1
 -- File: main.lua
--- Author: Tom Wright
+-- Author: Tom Wright, Oliver Wright
 -- Description: Main game loop
 ----------------------------------------------
+
+--------------------------------------------
+-- Loads assets at the beginning of the game
+function love.load()
+    ball = love.graphics.newImage("images/ball.png")
+    x = 200
+    y = 200
+end
 
 ------------------------------------
 -- Runs every frame, draw the screen
@@ -11,4 +19,5 @@ function love.draw()
     local width, height = love.graphics.getDimensions()
 
     love.graphics.print("Hello World", width/2, height/2)
+    love.graphics.draw(ball, x, y)
 end
